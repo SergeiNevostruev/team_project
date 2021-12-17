a664462777ea7f60098f6c3a2210e9bd3c31f324
Sergei Nevostruev <sergnevo@rambler.ru>
2021-12-17 09:14:57 +0300

    Корректированная карта товара (удалены дублирующие стили и шапка с футером вставлены последние с других файлов

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-// Look for .hamburger
-var hamburger = document.querySelector(".hamburger");
-// On click
-hamburger.addEventListener("click", function() {
-  // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
-  // Do something else, like open/close menu
-});
-=======
 
   //////-------------hamburger---------///////
     // Look for .hamburger
@@ -18,4 +8,3 @@ hamburger.addEventListener("click", function() {
       hamburger.classList.toggle("is-active");
       // Do something else, like open/close menu
     });
->>>>>>> 55a99b0b8f6db87bbaddc83fb3f732b2038b00ea
