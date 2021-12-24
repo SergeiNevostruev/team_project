776b5d7d0b1e9f7db79292346fd5112a9a012a5a
Nikita Tsipelev <npometall@mail.ru>
2021-12-24 10:04:48 +0300

    добавил выпадающее меню

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -22,3 +22,28 @@ function openCity(evt, cityName) {
   // При зарузке страницы показывает вседа активную вкладку
   document.getElementById("defaultOpen").click();
 
+
+
+  // Выпадающее меню
+
+document.addEventListener('DOMContentLoaded', () => { 
+
+  const button = document.querySelector('.dropbtn') // находим кнопку для открытия/закрытия окна навигации
+  const nav = document.querySelector('.show') // находим окно навигации
+  const svg = document.querySelector('.svg-top')
+
+  button.addEventListener('click', () => { // при клике на кнопку
+    nav.classList.toggle('show-active') // открываем/закрываем окно навигации, добавляя/удаляя активный класс
+    svg.classList.toggle('rotate')
+  })
+
+  window.addEventListener('click', e => { // при клике в любом месте окна браузера
+    const target = e.target // находим элемент, на котором был клик
+    if (!target.closest('.show') && !target.closest('.dropbtn')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+      nav.classList.remove('show-active') // то закрываем окно навигации, удаляя активный класс
+      svg.classList.remove('rotate')
+    }
+  })
+
+})
+
